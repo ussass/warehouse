4d9b5a06520727fa75007b8020ee21d5d022a493
Trofimov <mikletrof@gmail.com>
2021-09-24 16:03:35 +0300

    voided

--- src/main/java/ru/trofimov/warehouse/repositories/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.trofimov.warehouse.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import ru.trofimov.warehouse.model.Category;
-
-@Repository
-public interface CategoryRepository extends CrudRepository<Category, Long> {
-}
--- src/main/java/ru/trofimov/warehouse/repositories/ItemPositionRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.trofimov.warehouse.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import ru.trofimov.warehouse.model.ItemPosition;
-
-@Repository
-public interface ItemPositionRepository extends CrudRepository<ItemPosition, Long> {
-}
