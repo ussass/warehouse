f3cc3e27ac4b9a7043784593da45a5947a70d786
mikle <mikletrof@gmail.com>
2021-09-25 18:32:20 +0300

    added added new methods

--- src/main/java/ru/trofimov/warehouse/rest/CategoryRestController.java
+++ src/main/java/ru/trofimov/warehouse/rest/CategoryRestController.java
@@ -1,13 +1,14 @@
 package ru.trofimov.warehouse.rest;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.trofimov.warehouse.model.Category;
 import ru.trofimov.warehouse.service.CategoryService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/category")
@@ -31,5 +32,54 @@ public class CategoryRestController {
         }
 
         return new ResponseEntity<>(category, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Category> saveCategory(@RequestBody Category category){
+        HttpHeaders httpHeaders = new HttpHeaders();
+        System.out.println("category.toString() = " + category.toString());
+
+        if(category == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        categoryService.save(category);
+
+        return new ResponseEntity<>(category, httpHeaders, HttpStatus.OK);
+    }
+
+    @PutMapping("")
+    public ResponseEntity<Category> updateCategory(@RequestBody Category category){
+        HttpHeaders httpHeaders = new HttpHeaders();
+
+        if(category == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        categoryService.save(category);
+
+        return new ResponseEntity<>(category, httpHeaders, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Category> deleteCategory(@PathVariable Long id){
+        Category category = categoryService.findById(id);
+        if (category == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        categoryService.delete(id);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+
+    @GetMapping("")
+    public ResponseEntity<List<Category>> getAllCategory(){
+        List<Category> categories = categoryService.findAll();
+
+        if (categories.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(categories, HttpStatus.OK);
     }
 }
