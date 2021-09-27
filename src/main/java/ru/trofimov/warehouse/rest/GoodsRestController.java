bd5ca34acd028461badb19abf590820fe65fe07a
Trofimov <mikletrof@gmail.com>
2021-09-27 13:26:39 +0300

    added new methods

--- src/main/java/ru/trofimov/warehouse/rest/GoodsRestController.java
+++ src/main/java/ru/trofimov/warehouse/rest/GoodsRestController.java
@@ -1,4 +1,83 @@
 package ru.trofimov.warehouse.rest;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.trofimov.warehouse.model.Goods;
+import ru.trofimov.warehouse.service.GoodsService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/goods")
 public class GoodsRestController {
+    private final GoodsService goodsService;
+
+    public GoodsRestController(GoodsService goodsService) {
+        this.goodsService = goodsService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Goods> getGoods(@PathVariable Long id){
+        if (id == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        Goods goods = goodsService.findById(id);
+
+        if (goods == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(goods, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Goods> saveGoods(@RequestBody Goods goods){
+        HttpHeaders httpHeaders = new HttpHeaders();
+
+        if(goods == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        goodsService.save(goods);
+
+        return new ResponseEntity<>(goods, httpHeaders, HttpStatus.OK);
+    }
+
+    @PutMapping("")
+    public ResponseEntity<Goods> updateGoods(@RequestBody Goods goods){
+        HttpHeaders httpHeaders = new HttpHeaders();
+
+        if(goods == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        goodsService.save(goods);
+
+        return new ResponseEntity<>(goods, httpHeaders, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Goods> deleteGoods(@PathVariable Long id){
+        Goods goods = goodsService.findById(id);
+        if (goods == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        goodsService.delete(id);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+
+    @GetMapping("")
+    public ResponseEntity<List<Goods>> getAllGoods(){
+        List<Goods> goodsList = goodsService.findAll();
+
+        if (goodsList.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(goodsList, HttpStatus.OK);
+    }
 }
--- src/main/java/ru/trofimov/warehouse/rest/ItemPositionRestController.java
+++ src/main/java/ru/trofimov/warehouse/rest/ItemPositionRestController.java
@@ -1,4 +1,84 @@
 package ru.trofimov.warehouse.rest;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.trofimov.warehouse.model.ItemPosition;
+import ru.trofimov.warehouse.service.ItemPositionService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/item-position")
 public class ItemPositionRestController {
+    private final ItemPositionService itemPositionService;
+
+    public ItemPositionRestController(ItemPositionService itemPositionService) {
+        this.itemPositionService = itemPositionService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ItemPosition> getItemPosition(@PathVariable Long id){
+        if (id == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        ItemPosition itemPosition = itemPositionService.findById(id);
+
+        if (itemPosition == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(itemPosition, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ItemPosition> saveItemPosition(@RequestBody ItemPosition itemPosition){
+        HttpHeaders httpHeaders = new HttpHeaders();
+
+        if(itemPosition == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        itemPositionService.save(itemPosition);
+
+        return new ResponseEntity<>(itemPosition, httpHeaders, HttpStatus.OK);
+    }
+
+    @PutMapping("")
+    public ResponseEntity<ItemPosition> updateItemPosition(@RequestBody ItemPosition itemPosition){
+        HttpHeaders httpHeaders = new HttpHeaders();
+
+        if(itemPosition == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        itemPositionService.save(itemPosition);
+
+        return new ResponseEntity<>(itemPosition, httpHeaders, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ItemPosition> deleteItemPosition(@PathVariable Long id){
+        ItemPosition itemPosition = itemPositionService.findById(id);
+        if (itemPosition == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        itemPositionService.delete(id);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+
+    @GetMapping("")
+    public ResponseEntity<List<ItemPosition>> getAllItemPositions(){
+        List<ItemPosition> itemPositions = itemPositionService.findAll();
+
+        if (itemPositions.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(itemPositions, HttpStatus.OK);
+    }
+
 }
