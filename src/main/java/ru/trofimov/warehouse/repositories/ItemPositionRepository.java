248b5c9fc2aa493ebfab3648a9da21ccf81c37b7
Trofimov <mikletrof@gmail.com>
2021-09-27 16:35:11 +0300

    added new method

--- src/main/java/ru/trofimov/warehouse/repositories/ItemPositionRepository.java
+++ src/main/java/ru/trofimov/warehouse/repositories/ItemPositionRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.trofimov.warehouse.model.ItemPosition;
 
+import java.util.List;
+
 @Repository
 public interface ItemPositionRepository extends CrudRepository<ItemPosition, Long> {
+    List<ItemPosition> findByGoodsId(Long goodsId);
 }
