acfb38fbb6a671f6a557dbecd7e72a34c7842392
Trofimov <mikletrof@gmail.com>
2021-10-08 16:13:24 +0300

    added new class

---
+++ src/main/java/ru/trofimov/warehouse/security/jwt/JwtProvider.java
@@ -0,0 +1,48 @@
+package ru.trofimov.warehouse.security.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+
+    @Value("${jwt.token.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.token.expired}")
+    private long validity;
+
+
+    public String generateToken(String login) {
+        Date date = Date.from(LocalDate.now().plusDays(validity).atStartOfDay(ZoneId.systemDefault()).toInstant());
+        return Jwts.builder()
+                .setSubject(login)
+                .setExpiration(date)
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+//            log.severe("invalid token");
+            System.out.println("invalid token");
+        }
+        return false;
+    }
+
+    public String getLoginFromToken(String token) {
+        Claims claims = Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody();
+        return claims.getSubject();
+    }
+
+}
